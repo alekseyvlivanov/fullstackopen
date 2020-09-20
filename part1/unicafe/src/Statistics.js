4f6e42af38322c57cd2dfed595522044a5b2e22f
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-09-20 18:15:39 +1000

    feat: add task 1.7: unicafe step2 solution

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -2,11 +2,23 @@ import React from 'react';
 
 const Statistics = (props) => {
   const { feedback } = props;
-  return Object.keys(feedback).map((mark) => (
-    <div key={mark}>
-      {mark}: {feedback[mark]}
-    </div>
-  ));
+  const { good, neutral, bad } = feedback;
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const positive = all > 0 ? (good * 100) / all : 0;
+
+  return (
+    <>
+      {Object.keys(feedback).map((mark) => (
+        <div key={mark}>
+          {mark}: {feedback[mark]}
+        </div>
+      ))}
+      <div>all: {all}</div>
+      <div>average: {average}</div>
+      <div>positive: {positive}%</div>
+    </>
+  );
 };
 
 export default Statistics;
