6f42ba4371ad8147fef23aacb309db43bf54a8f6
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-09-24 00:50:56 +1000

    feat: add task 2.6: The Phonebook Step1 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,19 +4,31 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName('');
+  };
+
+  const handleChange = ({ target: { value } }) => {
+    setNewName(value);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
