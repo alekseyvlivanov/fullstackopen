4b066d6d7dd0b7899b39e6a20b8de9f1df51fdf4
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-09-23 22:53:26 +1000

    feat: add task 2.4: Course information step9 solution

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = ({ course }) => {
-  return <h1>{course.name}</h1>;
+const Title = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Header = ({ course: { name } }) => {
+  return <h2>{name}</h2>;
 };
 
 const Total = ({ course: { parts } }) => {
@@ -37,34 +41,60 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+  const title = 'Web development curriculum';
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <>
+      <Title title={title} />
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
