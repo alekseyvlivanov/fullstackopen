39071b9b0a3c04d01e069520cdccbf1ce150732c
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-09-28 13:46:56 +1000

    feat: add task 2.12 Data for countries, step1 solution

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -11,10 +11,48 @@ const Filter = ({ handleFilter }) => {
 
 const App = () => {
   const [countries, setCountries] = useState([]);
-  const [filterName, setFilterName] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [, setFilterName] = useState('');
+
+  const renderCounties = () => {
+    if (filteredCountries.length === 0) {
+      return null;
+    }
+
+    if (filteredCountries.length > 10) {
+      return <div>Too many matches, specify another filter</div>;
+    }
+
+    if (filteredCountries.length > 1) {
+      return filteredCountries.map((c) => <div key={c.name}>{c.name}</div>);
+    }
+
+    const { name, capital, population, languages, flag } = filteredCountries[0];
+
+    return (
+      <>
+        <h2>{name}</h2>
+        <div>capital {capital}</div>
+        <div>population {population}</div>
+        <h3>languages</h3>
+        <ul>
+          {languages.map((l) => (
+            <li key={l.name}>{l.name}</li>
+          ))}
+        </ul>
+        <img src={flag} width="150" alt="flag" />
+      </>
+    );
+  };
 
   const handleFilter = ({ target: { value } }) => {
     setFilterName(value);
+    const filteredValue = value.trim().toLowerCase();
+    const filteredData =
+      filteredValue === ''
+        ? []
+        : countries.filter((c) => c.name.toLowerCase().includes(filteredValue));
+    setFilteredCountries(filteredData);
   };
 
   const getCountries = () => {
@@ -28,6 +66,7 @@ const App = () => {
   return (
     <div>
       <Filter handleFilter={handleFilter} />
+      {renderCounties()}
     </div>
   );
 };
