bcd9404f015319d8bc56e39b12972a15fd54fbbe
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-02 09:11:26 +1000

    feat: add task 3.5 Phonebook backend step5 solution

--- backend/part3/index.js
+++ backend/part3/index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json());
 
+const generateId = () => {
+  return Math.round(Math.random() * 10000);
+};
+
 let persons = [
   {
     name: "Arto Hellas",
@@ -50,6 +54,26 @@ app.delete("/api/persons/:id", (req, res) => {
   persons = persons.filter((person) => person.id !== id);
 
   res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const { body } = req;
+
+  if (!body.name) {
+    return res.status(404).json({
+      error: "name missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
 });
 
 app.get("/info", (req, res) => {
