0a4b650bb48cbcac86c0301bf35349848dad0495
Aleksey Ivanov <aleksey.vl.ivanov@gmail.com>
2020-10-02 11:41:47 +1000

    fix: status code for missing data

--- backend/part3/index.js
+++ backend/part3/index.js
@@ -60,19 +60,19 @@ app.post("/api/persons", (req, res) => {
   const { body } = req;
 
   if (!body.name) {
-    return res.status(404).json({
+    return res.status(409).json({
       error: "name is missing",
     });
   }
 
   if (persons.find((person) => person.name === body.name)) {
-    return res.status(404).json({
+    return res.status(409).json({
       error: "name must be unique",
     });
   }
 
   if (!body.number) {
-    return res.status(404).json({
+    return res.status(409).json({
       error: "number is missing",
     });
   }
